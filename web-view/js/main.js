0d145ddf8654d565a56c482e04ef15e9a8584551
Schlauben <78811649+Schlauben@users.noreply.github.com>
2024-02-13 14:45:31 +0100

    Added ambient light and a tree

--- web-view/js/main.js
+++ web-view/js/main.js
@@ -1,9 +1,12 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { Sky } from 'three/addons/objects/Sky.js';
+import { FontLoader } from 'three/addons/loaders/FontLoader.js';
+import { TextGeometry } from 'three/addons/geometries/TextGeometry.js';
 
 let camera, scene, renderer, bulbMat, light;
-let ballMat, cubeMat, floorMat, lightMat;
+let ballMat, cubeMat, floorMat, drivingMat, car;
 
 let previousShadowMap = false;
 
@@ -21,61 +24,33 @@ function init() {
     const textureLoader = new THREE.TextureLoader();
 
     scene = new THREE.Scene();
+    // scene.background = new THREE.Color(0x87ceeb);
+
+    scene.add(generateSky());
+
     camera = setCamera();
     light = setLight(textureLoader);
-
     scene.add(light);
 
-    generateCube(textureLoader);
-
-
-    ballMat = new THREE.MeshStandardMaterial({
-        color: 0xffffff,
-        roughness: 0.5,
-        metalness: 1.0
-    });
-    textureLoader.load('js/img/sky.jpg', function (map) {
-
-        map.anisotropy = 4;
-        map.colorSpace = THREE.SRGBColorSpace;
-        ballMat.map = map;
-        ballMat.needsUpdate = true;
-
-    });
+    createFence();
 
     scene.add(generateFloor(textureLoader));
-    // scene.add(generateSky(textureLoader));
-
-    const ballGeometry = new THREE.SphereGeometry(0.25, 32, 32);
-    const ballMesh = new THREE.Mesh(ballGeometry, ballMat);
-    ballMesh.position.set(1, 0.25, 1);
-    ballMesh.rotation.y = Math.PI;
-    ballMesh.castShadow = true;
-    scene.add(ballMesh);
-
-    const boxGeometry = new THREE.BoxGeometry(0.5, 0.5, 0.5);
-    const boxMesh = new THREE.Mesh(boxGeometry, cubeMat);
-    boxMesh.position.set(- 0.5, 0.25, - 1);
-    boxMesh.castShadow = true;
-    scene.add(boxMesh);
-
-    const boxMesh2 = new THREE.Mesh(boxGeometry, cubeMat);
-    boxMesh2.position.set(0, 0.25, - 5);
-    boxMesh2.castShadow = true;
-    scene.add(boxMesh2);
-
-    const boxMesh3 = new THREE.Mesh(boxGeometry, cubeMat);
-    boxMesh3.position.set(7, 0.25, 0);
-    boxMesh3.castShadow = true;
-    scene.add(boxMesh3);
+    scene.add(generateDrivingArea(textureLoader));
 
     const loader = new GLTFLoader()
     // loading model
     loader.load('js/img/car/scene.gltf', (gltf) => {
-        const batman = gltf.scene;
-        batman.scale.setScalar(0.005);
-        batman.position.x = 10;
-        scene.add(batman)
+        car = gltf.scene;
+        car.scale.setScalar(0.005);
+        car.position.x = 10;
+        scene.add(car)
+    })
+
+    loader.load('js/img/tree/scene.gltf', (gltf) => {
+        let house = gltf.scene;
+        house.scale.setScalar(2);
+        house.position.x = 30;
+        scene.add(house)
     })
 
 
@@ -92,43 +67,18 @@ function init() {
 
 function setCamera() {
     camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 100);
-    camera.position.x = - 4;
+    camera.position.x = -4;
     camera.position.z = 4;
     camera.position.y = 2;
     return camera;
 }
 
-function setLight(textureLoader) {
-    const bulbGeometry = new THREE.SphereGeometry(1, 16, 8);
-
-    // Erstelle ein Mesh mit der Glühbirnen-Textur
-    bulbMat = new THREE.MeshStandardMaterial({
-        emissive: 0xffffee,
-        emissiveIntensity: 10
-    });
-
-    textureLoader.load('js/img/sky.jpg', function (map) {
-        map.colorSpace = THREE.SRGBColorSpace;
-        bulbMat.map = map;
-        bulbMat.needsUpdate = true;
-    });
-
-    const bulbMesh = new THREE.Mesh(bulbGeometry, bulbMat);
-
-    // Erstelle eine PointLight und füge das Mesh als Child hinzu
-    const bulbLight = new THREE.PointLight(0xffee88, 1, 100, 2);
-    bulbLight.add(bulbMesh);
-
-    // Positioniere die Lichtquelle
-    bulbLight.position.set(5, 5, -5);
-    bulbLight.castShadow = true;
-    bulbLight.power = 18000;
-
-    return bulbLight;
+function setLight() {
+    const light = new THREE.AmbientLight(0xffffff, 50)
+    return light;
 }
 
 function onWindowResize() {
-
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
@@ -139,6 +89,55 @@ function setControls() {
     controls.minDistance = 1;
     controls.maxDistance = 20;
     return controls;
+}
+
+function createText(text, position) {
+    var loader = new FontLoader();
+
+    loader.load('https://threejsfundamentals.org/threejs/resources/threejs/fonts/helvetiker_regular.typeface.json', function (font) {
+        var textGeometry = new TextGeometry(text, {
+            font: font,
+            size: 0.2,
+            height: 0.05,
+        });
+
+        var textMaterial = new THREE.MeshBasicMaterial({ color: 0x000000 });
+        var textMesh = new THREE.Mesh(textGeometry, textMaterial);
+        textMesh.position.copy(position);
+        scene.add(textMesh);
+    });
+}
+
+
+function createFence() {
+    // Erstellen Sie den Zaun
+    var fenceGeometry = new THREE.BoxGeometry(0.1, 1, 0.1); // Ändern Sie die Größe entsprechend Ihren Anforderungen
+    var fenceMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+
+    for (var i = -10; i <= 10; i++) {
+        var fence = new THREE.Mesh(fenceGeometry, fenceMaterial);
+        fence.position.set(0, 0.5, i); // Platzieren Sie den Zaun entsprechend
+        scene.add(fence);
+    }
+
+    for (var i = -9; i < 10; i++) {
+        var fence = new THREE.Mesh(fenceGeometry, fenceMaterial);
+        fence.position.set(20, 0.5, i); // Platzieren Sie den Zaun entsprechend
+        scene.add(fence);
+    }
+
+    for (var i = 0; i <= 20; i++) {
+        var fence = new THREE.Mesh(fenceGeometry, fenceMaterial);
+        fence.position.set(i, 0.5, -10); // Platzieren Sie den Zaun entsprechend
+        scene.add(fence);
+    }
+
+    for (var i = 0; i <= 20; i++) {
+        var fence = new THREE.Mesh(fenceGeometry, fenceMaterial);
+        fence.position.set(i, 0.5, 10); // Platzieren Sie den Zaun entsprechend
+        scene.add(fence);
+    }
+
 }
 
 function generateFloor(textureLoader) {
@@ -158,47 +157,57 @@ function generateFloor(textureLoader) {
         floorMat.needsUpdate = true;
 
     });
-    const floorGeometry = new THREE.PlaneGeometry(100, 100);
+    const floorGeometry = new THREE.PlaneGeometry(400, 400);
     const floorMesh = new THREE.Mesh(floorGeometry, floorMat);
     floorMesh.receiveShadow = true;
     floorMesh.rotation.x = - Math.PI / 2.0;
     return floorMesh;
 }
 
-function generateCube(textureLoader) {
-    cubeMat = new THREE.MeshStandardMaterial({
-        roughness: 0.7,
+function generateDrivingArea(textureLoader) {
+
+    drivingMat = new THREE.MeshStandardMaterial({
+        roughness: 0.8,
         color: 0xffffff,
-        bumpScale: 1,
-        metalness: 0.2
+        metalness: 0.2,
+        bumpScale: 1
     });
-
-    textureLoader.load('js/img/sky.jpg', function (map) {
-
+    textureLoader.load('js/img/street.jpg', function (map) {
         map.wrapS = THREE.RepeatWrapping;
         map.wrapT = THREE.RepeatWrapping;
-        map.anisotropy = 4;
-        map.repeat.set(1, 1);
+        map.repeat.set(10, 10);
         map.colorSpace = THREE.SRGBColorSpace;
-        cubeMat.map = map;
-        cubeMat.needsUpdate = true;
+        drivingMat.map = map;
+        drivingMat.needsUpdate = true;
 
     });
+    const floorGeometry = new THREE.PlaneGeometry(20, 20);
+    const floorMesh = new THREE.Mesh(floorGeometry, drivingMat);
+    floorMesh.position.x = 10;
+    floorMesh.position.y = 0.0001;
+    floorMesh.receiveShadow = true;
+    floorMesh.rotation.x = - Math.PI / 2.0;
+    return floorMesh;
 }
 
-function generateSky(textureLoader) {
-    const skyboxTexture = textureLoader.load([
-        'js/img/night.jpg'
-    ]);
+function generateSky() {
+    const sky = new Sky();
+    sky.scale.setScalar(10000);
 
-    const skyboxMaterial = new THREE.MeshBasicMaterial({
-        map: skyboxTexture,
-        side: THREE.BackSide, // Wichtig, um die Skybox von innen zu sehen
-    });
+    const skyUniforms = sky.material.uniforms;
 
-    const skyboxGeometry = new THREE.BoxGeometry(100, 100, 100);
-    const skyboxMesh = new THREE.Mesh(skyboxGeometry, skyboxMaterial);
-    return skyboxMesh;
+    skyUniforms['turbidity'].value = 10;
+    skyUniforms['rayleigh'].value = 2;
+    skyUniforms['mieCoefficient'].value = 0.005;
+    skyUniforms['mieDirectionalG'].value = 0.8;
+    const phi = THREE.MathUtils.degToRad(-80);
+    const theta = THREE.MathUtils.degToRad(-20);
+    let sun = new THREE.Vector3();
+
+    sun.setFromSphericalCoords(1, phi, theta);
+
+    sky.material.uniforms['sunPosition'].value.copy(sun);
+    return sky;
 }
 
 //
@@ -213,13 +222,13 @@ function animate() {
 function render() {
 
     renderer.toneMappingExposure = Math.pow(params.exposure, 5.0); // to allow for very bright scenes.
-    light.castShadow = params.shadows;
+    // light.castShadow = params.shadows;
 
     if (params.shadows !== previousShadowMap) {
-        ballMat.needsUpdate = true;
-        cubeMat.needsUpdate = true;
         floorMat.needsUpdate = true;
         previousShadowMap = params.shadows;
     }
+    if (car && car.position)
+        car.position.z -= 0.01;
     renderer.render(scene, camera);
 }
