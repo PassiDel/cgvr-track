3f015184115a0ee5aaa8a72c51c9a2943709c655
Pascal Syma <pascal@syma.dev>
2024-02-20 11:46:04 +0100

    add realtime

--- calib.py
+++ calib.py
@@ -1,18 +1,20 @@
-data = [
-    [(584.2385786802031, 172.46192893401016), (682.0, 233.95939086294416), (773.010152284264, 287.98477157360406),
-     (859.015306122449, 337.0), (498.0, 271.3265306122449), (597.3128205128205, 324.0), (689.7179487179487, 369.0),
-     (776.0204081632653, 411.9642857142857), (417.0, 355.0), (520.0, 403.0051020408163), (611.2233502538071, 440.0),
-     (696.0, 480.0)],
-    [(544.0, 296.0), (480.0, 368.0), (-1.0, -1.0), (376.0, 488.0), (408.0, 264.0), (352.0, 344.0), (312.0, 408.0),
-     (-1.0, -1.0), (269.1015228426396, 230.0), (216.0, 314.0), (184.0, 384.0), (-1.0, -1.0)],
-    [(317.995, 196.34), (448.5925, 161.1875), (592.42, 119.0), (755.1975, 75.5), (367.0, 337.04),
-     (498.45137157107234, 303.02493765586036), (639.0, 267.815), (800.215, 219.175), (407.0, 468.0), (542.0, 441.0),
-     (676.9625935162095, 404.8927680798005), (830.9501246882793, 370.9800498753117)]]
+# data = [
+#     [(584.2385786802031, 172.46192893401016), (682.0, 233.95939086294416), (773.010152284264, 287.98477157360406),
+#      (859.015306122449, 337.0), (498.0, 271.3265306122449), (597.3128205128205, 324.0), (689.7179487179487, 369.0),
+#      (776.0204081632653, 411.9642857142857), (417.0, 355.0), (520.0, 403.0051020408163), (611.2233502538071, 440.0),
+#      (696.0, 480.0)],
+#     [(544.0, 296.0), (480.0, 368.0), (-1.0, -1.0), (376.0, 488.0), (408.0, 264.0), (352.0, 344.0), (312.0, 408.0),
+#      (-1.0, -1.0), (269.1015228426396, 230.0), (216.0, 314.0), (184.0, 384.0), (-1.0, -1.0)],
+#     [(317.995, 196.34), (448.5925, 161.1875), (592.42, 119.0), (755.1975, 75.5), (367.0, 337.04),
+#      (498.45137157107234, 303.02493765586036), (639.0, 267.815), (800.215, 219.175), (407.0, 468.0), (542.0, 441.0),
+#      (676.9625935162095, 404.8927680798005), (830.9501246882793, 370.9800498753117)]]
 
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib as mpl
+import ast
+from os import listdir
 
 chessboard_size = (4, 3)
 
@@ -23,69 +25,108 @@ colors = range(len(objp))
 
 imageSize = (1024, 768)
 
+calib_files = [
+    ['weiss_raum', 'weiss_raum_2'],
+    ['schwarz', 'schwarz_2'],
+    ['weiss_fenster', 'weiss_fenster_2']
+]
+
+stereo_calib_files = listdir('stereo_improved')
+
 
 def main():
-    print(len(data[0]), len(objp))
-
-    fig = plt.figure()
-    gs = fig.add_gridspec(len(data), hspace=0)
-    axs = gs.subplots(sharex=True, sharey=True)
-
-    for i in range(len(axs)):
-        ax = axs[i]
-        ax.set_xlim((0, 1024))
-        ax.set_ylim((0, 768))
-        x, y = np.array(data[i]).T
-        ax.scatter(x, y, c=colors)
-        # ax.plot(x, y)
-        for j in range(len(objp)):
-            ax.text(data[i][j][0], data[i][j][1] + 30, str(objp[j]), horizontalalignment='center')
-
-    cam1ImgPoints, cam1ObjPoints = getPointsForCamera(0)
-    cam2ImgPoints, cam2ObjPoints = getPointsForCamera(1)
-    cam3ImgPoints, cam3ObjPoints = getPointsForCamera(2)
-
-    # Calibrate each camera for its intrinsic and extrinsic parameters
-    error1, cam1Matrix, cam1DistCoeff, _, _ = cv2.calibrateCamera(cam1ObjPoints, cam1ImgPoints, imageSize,
-                                                                  np.zeros((3, 3), np.float32), np.zeros(4, np.float32))
-    error2, cam2Matrix, cam2DistCoeff, _, _ = cv2.calibrateCamera(cam2ObjPoints, cam2ImgPoints, imageSize,
-                                                                  np.zeros((3, 3), np.float32), np.zeros(4, np.float32))
-    error3, cam3Matrix, cam3DistCoeff, _, _ = cv2.calibrateCamera(cam3ObjPoints, cam3ImgPoints, imageSize,
-                                                                  np.zeros((3, 3), np.float32), np.zeros(4, np.float32))
-
-    print(error1, error2, error3)
+    cam1_dist_coeff, cam1_matrix = calibrate_camera(calib_files[0])
+    # cam2_dist_coeff, cam2_matrix = calibrate_camera(calib_files[1])
+    cam3_dist_coeff, cam3_matrix = calibrate_camera(calib_files[2])
 
     # Calibrate two cameras to each other
-    cam12_1ImgPoints, cam12_2ImgPoints, objPoints12 = getPointsForCameras(0, 1)
-    cam13_1ImgPoints, cam13_2ImgPoints, objPoints13 = getPointsForCameras(0, 2)
-    error12, _, _, _, _, r12, t12, e12, f12 = cv2.stereoCalibrate(objPoints12, cam12_1ImgPoints, cam12_2ImgPoints,
-                                                                  cam1Matrix, cam1DistCoeff,
-                                                                  cam2Matrix, cam2DistCoeff, imageSize)
-    error13, _, _, _, _, r13, t13, e13, f13 = cv2.stereoCalibrate(objPoints13, cam13_1ImgPoints, cam13_2ImgPoints,
-                                                                  cam2Matrix, cam2DistCoeff,
-                                                                  cam3Matrix, cam3DistCoeff, imageSize)
-    print(error12, error13)
-
     # Rectify (?) each two-camera calibration
-    _, _, p12_1, p12_2, q12, _, _ = cv2.stereoRectify(cam1Matrix, cam1DistCoeff, cam2Matrix, cam2DistCoeff, imageSize,
-                                                      r12, t12)
-    _, _, p13_1, p13_2, q13, _, _ = cv2.stereoRectify(cam1Matrix, cam1DistCoeff, cam3Matrix, cam3DistCoeff, imageSize,
-                                                      r13, t13)
+    # p12_1, p12_2, q12 = stereo_calibrate(0, 1,
+    #                                      cam1_matrix, cam1_dist_coeff,
+    #                                      cam2_matrix, cam2_dist_coeff)
+    # p13_1, p13_2, q13 = stereo_calibrate(2, 1,
+    #                                      cam3_matrix, cam3_dist_coeff,
+    #                                      cam2_matrix, cam2_dist_coeff)
+    p13_1, p13_2, q13 = stereo_calibrate(0, 2,
+                                         cam1_matrix, cam1_dist_coeff,
+                                         cam3_matrix, cam3_dist_coeff)
+    print(homogeneous_to_cartesian(
+        cv2.triangulatePoints(p12_1, p12_2, np.array(data[1][2]).T, np.array(data[0][2]).T)))
 
-    print(p12_1, p12_2, q12)
+    # print(p12_1, p12_2, q12)
     print(p13_1, p13_2, q13)
 
-    # Triangulate 3d world points, based on the two-camera calibration
-    # This step can run in realtime
-    wp12 = cv2.triangulatePoints(p12_1, p12_2, np.array(data[0][0]).T, np.array(data[1][0]).T)
-    wp13 = cv2.triangulatePoints(p13_1, p13_2, np.array(data[0][0]).T, np.array(data[2][0]).T)
+    # with open(f'stereo_improved/{stereo_calib_files[0]}', 'r') as f:
+    #     data = ast.literal_eval(f.read())
+    #
+    #     fig = plt.figure()
+    #     gs = fig.add_gridspec(len(data), hspace=0)
+    #     axs = gs.subplots(sharex=True, sharey=True)
+    #
+    #     for i in range(len(axs)):
+    #         ax = axs[i]
+    #         ax.set_xlim((0, 1024))
+    #         ax.set_ylim((0, 768))
+    #         x, y = np.array(data[i]).T
+    #         ax.scatter(x, y, c=colors)
+    #         # ax.plot(x, y)
+    #         for j in range(len(objp)):
+    #             ax.text(data[i][j][0], data[i][j][1] + 30, str(objp[j]), horizontalalignment='center')
+    #
+    #     # Triangulate 3d world points, based on the two-camera calibration
+    #     # This step can run in realtime
+    #     wp12 = cv2.triangulatePoints(p12_1, p12_2, np.array(data[0][0]).T, np.array(data[1][0]).T)
+    #     wp13 = cv2.triangulatePoints(p13_1, p13_2, np.array(data[2][0]).T, np.array(data[1][0]).T)
+    #
+    #     cp12 = homogeneous_to_cartesian(wp12)
+    #     cp13 = homogeneous_to_cartesian(wp13)
+    #     dist = np.sqrt(np.sum(np.array(cp12) - np.array(cp13)) ** 2)
+    #     print("WELT:")
+    #     print(dist, cp12, cp13)
+    #     plt.show()
+    #     print(homogeneous_to_cartesian(
+    #         cv2.triangulatePoints(p12_1, p12_2, np.array(data[1][1]).T, np.array(data[0][1]).T)))
+    #     print(homogeneous_to_cartesian(
+    #         cv2.triangulatePoints(p12_1, p12_2, np.array(data[1][2]).T, np.array(data[0][2]).T)))
 
-    cp12 = homogeneous_to_cartesian(wp12)
-    cp13 = homogeneous_to_cartesian(wp13)
-    dist = np.sqrt(np.sum(np.array(cp12) - np.array(cp13)) ** 2)
-    print("WELT:")
-    print(dist, cp12, cp13)
-    plt.show()
+
+def calibrate_camera(files: list[str]):
+    """
+    Calibrate each camera for its intrinsic and extrinsic parameters
+    :param files: list of file names in ./wii_calib folder
+    :return: (cam_dist_coeff, cam_matrix)
+    """
+    cam_img_points, cam_obj_points = getPointsForCamera(files)
+    error, cam_matrix, cam_dist_coeff, _, _ = cv2.calibrateCamera(cam_obj_points, cam_img_points, imageSize,
+                                                                  np.zeros((3, 3), np.float32), np.zeros(4, np.float32))
+    print('error', error)
+    return cam_dist_coeff, cam_matrix
+
+
+def stereo_calibrate(idx1: int, idx2: int, cam1_matrix, cam1_dist_coeff,
+                     cam2_matrix, cam2_dist_coeff):
+    """
+    Calibrate two cameras to each other
+    Rectify (?) each two-camera calibration
+    :param ix1: index of first camera
+    :param idx2: index of second camera
+    :param cam1_matrix: first camera matrix
+    :param cam1_dist_coeff: first camera distance coefficients
+    :param cam2_matrix: second camera matrix
+    :param cam2_dist_coeff: second camera distance coefficients
+    :return: (p1, p2, q)
+    """
+
+    cam12_1_img_points, cam12_2_img_points, obj_points12 = getPointsForCameras(idx1, idx2)
+    error, _, _, _, _, r, t, e, f = cv2.stereoCalibrate(obj_points12, cam12_1_img_points, cam12_2_img_points,
+                                                        cam1_matrix, cam1_dist_coeff,
+                                                        cam2_matrix, cam2_dist_coeff, imageSize)
+
+    _, _, p1, p2, q, _, _ = cv2.stereoRectify(cam1_matrix, cam1_dist_coeff, cam2_matrix, cam2_dist_coeff,
+                                              imageSize,
+                                              r, t)
+    print('stereo error', idx1, idx2, len(obj_points12), error)
+    return p1, p2, q
 
 
 def homogeneous_to_cartesian(wp: [[float], [float], [float], [float]]):
@@ -93,39 +134,53 @@ def homogeneous_to_cartesian(wp: [[float], [float], [float], [float]]):
     return [x / w, y / w, z / w]
 
 
-def getPointsForCamera(index: int):
+def getPointsForCamera(files: list[str]):
     camObjPoints = []
     camImgPoints = []
     imgPoint = []
     objPoint = []
-    for j in range(len(data[index])):
-        (x, y) = data[index][j]
-        if x > -1 and y > -1:
-            imgPoint.append([x, y])
-            objPoint.append(objp[j])
-    camImgPoints.append(np.array(imgPoint, dtype=np.float32))
-    camObjPoints.append(np.array(objPoint, dtype=np.float32))
+    for file in files:
+        with open(f'wii_calib/{file}.txt', 'r') as f:
+            data = ast.literal_eval(f.read())
+            for j in range(len(data[0])):
+                (x, y) = data[0][j]
+                if x > -1 and y > -1:
+                    imgPoint.append([x, y])
+                    objPoint.append(objp[j])
+            camImgPoints.append(np.array(imgPoint, dtype=np.float32))
+            camObjPoints.append(np.array(objPoint, dtype=np.float32))
     return camImgPoints, camObjPoints
 
 
-def getPointsForCameras(index1: int, index2: int):
+def getPointsForCameras(idx1: int, idx2: int):
     objPoints = []
-    objPoint = []
 
     cam1ImgPoints = []
+    objPoint = []
     img1Point = []
-    cam2ImgPoints = []
     img2Point = []
-    for j in range(len(data[index1])):
-        (x1, y1) = data[index1][j]
-        (x2, y2) = data[index2][j]
-        if x1 > -1 and y1 > -1 and x2 > -1 and y2 > -1:
-            img1Point.append([x1, y1])
-            img2Point.append([x2, y2])
-            objPoint.append(objp[j])
-    cam1ImgPoints.append(np.array(img1Point, dtype=np.float32))
-    cam2ImgPoints.append(np.array(img2Point, dtype=np.float32))
-    objPoints.append(np.array(objPoint, dtype=np.float32))
+    cam2ImgPoints = []
+
+    for file in stereo_calib_files:
+        with open(f'stereo_improved/{file}', 'r') as f:
+            data = ast.literal_eval(f.read())
+            j = 0
+            for d1, d2 in zip(data[idx1], data[idx2]):
+                (x1, y1) = d1
+                (x2, y2) = d2
+                if x1 > -1 and y1 > -1 and x2 > -1 and y2 > -1:
+                    img1Point.append([x1, y1])
+                    img2Point.append([x2, y2])
+                    objPoint.append(objp[j])
+                j += 1
+        print(len(objPoint))
+        if len(objPoint) > 0:
+            cam1ImgPoints.append(np.array(img1Point, dtype=np.float32))
+            cam2ImgPoints.append(np.array(img2Point, dtype=np.float32))
+            objPoints.append(np.array(objPoint, dtype=np.float32))
+            objPoint = []
+            img1Point = []
+            img2Point = []
     return cam1ImgPoints, cam2ImgPoints, objPoints
 
 
---
+++ realtime.py
@@ -0,0 +1,59 @@
+import asyncio
+import sys
+
+import cv2
+import numpy as np
+
+from Wiimote import Wiimote
+from calib import calibrate_camera, stereo_calibrate, homogeneous_to_cartesian
+
+calib_files = [
+    ['weiss_raum', 'weiss_raum_2'],
+    ['schwarz', 'schwarz_2'],
+    ['weiss_fenster', 'weiss_fenster_2']
+]
+
+p13_1, p13_2, q13 = (0, 0, 0)
+
+amount = 2
+cache: list[list[tuple[float, float]]] = []
+
+
+def calibrate():
+    global p13_1, p13_2, q13
+    cam1_dist_coeff, cam1_matrix = calibrate_camera(calib_files[0])
+    cam3_dist_coeff, cam3_matrix = calibrate_camera(calib_files[2])
+
+    p13_1, p13_2, q13 = stereo_calibrate(0, 2,
+                                         cam1_matrix, cam1_dist_coeff,
+                                         cam3_matrix, cam3_dist_coeff)
+
+
+async def main():
+    global cache
+    wiimotes = [Wiimote(i + 1) for i in range(amount)]
+    for wiimote in wiimotes:
+        wiimote.subscribe(callback)
+
+    cache = [[] for _ in range(amount)]
+
+
+def callback(id: int, data: list[tuple[float, float]]):
+    global cache
+    if len(data) < 1:
+        return
+    cache[id].append(data[0])
+    if len(cache[0 if id == 1 else 1]) > 0:
+        print(homogeneous_to_cartesian(
+            cv2.triangulatePoints(p13_1, p13_2, np.array(cache[0][0]).T, np.array(cache[1][0]).T)))
+        cache = [[] for _ in range(amount)]
+
+
+if __name__ == '__main__':
+    calibrate()
+    loop = asyncio.get_event_loop()
+    try:
+        asyncio.ensure_future(main())
+        loop.run_forever()
+    finally:
+        loop.close()
